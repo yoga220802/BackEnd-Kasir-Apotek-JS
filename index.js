601c6facb613d6aa73153c4298cb499bc38e7579
yoga220802 <yogaagustiansyah01@gmail.com>
2024-12-17 07:25:08 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get('/home', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+});
+
+app.get('/', (req, res) => {
+  res.status(200).json('Welcome Home');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+module.exports = app;
