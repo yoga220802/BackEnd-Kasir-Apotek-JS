2e337ecfa8f6a55d540eb27f4f8426152561e64c
yoga220802 <yogaagustiansyah01@gmail.com>
2024-12-18 08:09:33 +0700

    feat: add CORS middleware to enable cross-origin requests
    - Integrated CORS middleware to handle cross-origin resource sharing.
    - Allows frontend (http://localhost:3000) to interact with backend APIs.
    - Resolves CORS policy errors when making requests from a different origin.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const sequelize = require('./src/config/database');
-const routes = require('./src/routes');
-
+const express = require("express");
+const dotenv = require("dotenv");
+const sequelize = require("./src/config/database");
+const routes = require("./src/routes");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -10,25 +10,28 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// Add cors middleware
+app.use(cors());
+
 // Register all routes
-app.use('/', routes);
+app.use("/", routes);
 
 // Database connection and server start
 (async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Database connection established.');
+ try {
+  await sequelize.authenticate();
+  console.log("Database connection established.");
 
-    await sequelize.sync({ force: false });
-    console.log('Models synced successfully.');
+  await sequelize.sync({ force: false });
+  console.log("Models synced successfully.");
 
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (err) {
-    console.error('Failed to connect to the database:', err);
-  }
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`);
+  });
+ } catch (err) {
+  console.error("Failed to connect to the database:", err);
+ }
 })();
 
 module.exports = app;
