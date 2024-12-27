ac2d617d0930d55cc7260178fb83c36947adfec4
yoga220802 <yogaagustiansyah01@gmail.com>
2024-12-27 19:44:49 +0700

    refactor(models): reorganize user and role models into a users directory

--- src/controllers/usermanager/editUserController.js
+++ src/controllers/usermanager/editUserController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
-const User = require('../../models/user');
-const Role = require('../../models/role');
+const User = require('../../models/users/user');
+const Role = require('../../models/users/role');
 
 // Edit user
 exports.editUser = async (req, res) => {
