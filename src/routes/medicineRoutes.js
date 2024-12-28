b68831f06660988bd59fb4d057ed79df41f8d5c6
yoga220802 <yogaagustiansyah01@gmail.com>
2024-12-28 10:12:08 +0700

    feat(medicine): add endpoints for retrieving medicine batches and batch details

--- src/routes/medicineRoutes.js
+++ src/routes/medicineRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { getMedicines, getMedicineById } = require('../controllers/medicineManager/getMedicineController');
+const { getMedicineBatches, getMedicineBatchById } = require('../controllers/medicineManager/getMedicineBatchController');
 const { verifyToken, authorizeRole } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 // GET /medicine
 router.get('/', verifyToken, authorizeRole('WRH'), getMedicines);
+router.get('/batch', verifyToken, authorizeRole('WRH'), getMedicineBatches);
+router.get('/batch/:batchid', verifyToken, authorizeRole('WRH'), getMedicineBatchById);
 router.get('/:medicineid', verifyToken, authorizeRole('WRH'), getMedicineById);
 
 module.exports = router;
