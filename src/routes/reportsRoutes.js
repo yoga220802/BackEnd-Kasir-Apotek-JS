b094c4cb0ec1bd8573d099a641af51b02634e594
yoga220802 <yogaagustiansyah01@gmail.com>
2025-01-11 11:38:33 +0700

    feat(reports): update sales and stock routes to include role-based authorization

--- src/routes/reportsRoutes.js
+++ src/routes/reportsRoutes.js
@@ -4,9 +4,9 @@ const { getSalesReport, exportSalesReport } = require('../controllers/reports/ge
 const { getStockReport, exportStockReport } = require('../controllers/reports/getStockReportController');
 const router = express.Router();
 
-router.get('/sales', verifyToken, getSalesReport);
-router.get('/sales/export', verifyToken, exportSalesReport);
-router.get('/stock', verifyToken, getStockReport);
+router.get('/sales', verifyToken, authorizeRole('ADM', 'CSR'), getSalesReport);
+router.get('/sales/export', verifyToken, authorizeRole('ADM', 'CSR'), exportSalesReport);
+router.get('/stock', verifyToken, authorizeRole('ADM', 'WRH'),getStockReport);
 router.get('/stock/export', verifyToken, authorizeRole('ADM', 'WRH'), exportStockReport);
 
 module.exports = router;
